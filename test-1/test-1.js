586e990511b04b8b69e13f47f7da86846db2e7a2
Carlos Gorrin Suarez <csuarez@Lenovo.com>
2017-10-09 00:11:03 -0300

    progress test-1

--- test-1/test-1.js
+++ test-1/test-1.js
@@ -1,4 +1,19 @@
 var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
+var until = webdriver.until;
 
-var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
-driver.get('http://webdriver.io');
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+
+driver.get('http://webdriver.io')
+    .then(_ => 
+    	driver(title('WebdriverIO - WebDriver bindings for Node.js'), 1000))
+    .then(_ =>
+        driver.findElement(By.className('header')))
+    .then(_ =>
+        driver.findElementBy.tagName('input').sendKeys('THE BROWSER OBJECT', Key.RETURN))
+    .then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 1000))
+    console.log('Working properly!');
+   
